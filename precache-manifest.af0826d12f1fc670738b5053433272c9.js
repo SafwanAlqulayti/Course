a9cd9e391bda270f423e2bd21e38631717fdb2c6
SafwanAlqulayti <safwanalqulayti@gmail.com>
2020-04-15 13:41:41 +0300

    Updates

---
+++ precache-manifest.af0826d12f1fc670738b5053433272c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ad4cd9a5ae8e962503000c38577f4a",
+    "url": "/course/index.html"
+  },
+  {
+    "revision": "3dcbdb1999aecad06fcf",
+    "url": "/course/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e2e04172f85e4e731987",
+    "url": "/course/static/css/main.ade02319.chunk.css"
+  },
+  {
+    "revision": "3dcbdb1999aecad06fcf",
+    "url": "/course/static/js/2.24ebfb0d.chunk.js"
+  },
+  {
+    "revision": "dbc2e0a72086f9a12686912e3cfb9730",
+    "url": "/course/static/js/2.24ebfb0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2e04172f85e4e731987",
+    "url": "/course/static/js/main.a1c5b031.chunk.js"
+  },
+  {
+    "revision": "afb73139c5dc6b986881",
+    "url": "/course/static/js/runtime-main.f14e60b7.js"
+  },
+  {
+    "revision": "46aaeee7f3affabfbd7c7c699c8db813",
+    "url": "/course/static/media/logo.46aaeee7.jpg"
+  }
+]);
